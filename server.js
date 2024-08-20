0fbb8675a60609594fc0a11aefa76878f43e2d01
Bruce1995010101 <bruceliu84@gmail.com>
2024-08-20 11:53:07 +0800

    去除path

--- server.js
+++ server.js
@@ -265,8 +265,8 @@ app.get('/logout', function (req, res) {
     res.send('out')
 })
 
-app.use((req, res, next) => {
-    console.log(`404 Error: ${req.originalUrl}`);
-    res.status(404).sendFile(path.join(__dirname, 'public', 'error.html'));
-});
+// app.use((req, res, next) => {
+//     console.log(`404 Error: ${req.originalUrl}`);
+//     res.status(404).sendFile(path.join(__dirname, 'public', 'error.html'));
+// });
 
